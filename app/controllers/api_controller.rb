8382ff16f5fa45064256ae048c847a50e31f8447
Evan Wheeler <anon0mys87@gmail.com>
2018-06-26 15:36:10 -0600

    Adds api controller record not found error handling

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,8 @@
 class ApiController < ActionController::API
+  include ActionController::Helpers
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render status: 404, json: {}
+  end
 end
--- spec/requests/api/v1/foods/food_endpoint_spec.rb
+++ spec/requests/api/v1/foods/food_endpoint_spec.rb
@@ -10,4 +10,10 @@ describe 'GET /api/v1/foods/:id' do
 
     expect(response.body).to eq(food.to_json)
   end
+
+  it 'returns a 404 status if food not found' do
+    get "/api/v1/foods/200"
+
+    expect(response.status).to eq(404)
+  end
 end
