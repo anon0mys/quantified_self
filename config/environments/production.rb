a393f952f2ab2e3738b78c2d30f3bd19ec30591d
Evan Wheeler <anon0mys87@gmail.com>
2018-06-27 08:53:17 -0600

    Rename project directory and adjust config for AWS deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "quantified_self_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "qs_rails_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
