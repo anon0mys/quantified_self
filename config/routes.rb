095dc515385f8c0fc824122958017d3b47dcd58b
Evan Wheeler <anon0mys87@gmail.com>
2018-06-27 20:13:17 -0600

    Adds meal endpoint with associated foods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :foods, only: %i[index show create update destroy]
       resources :meals, only: %i[index] do
         post '/foods/:id', to: 'meals/foods#create'
+        get '/foods', to: 'meals#show'
       end
     end
   end
