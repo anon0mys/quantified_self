941e3e4d776a839aa742de0f6922bbc1be285b51
Evan Wheeler <anon0mys87@gmail.com>
2018-06-27 20:08:23 -0600

    Updates meals test to handle multiple meals on index

--- spec/requests/api/v1/meals/meals_endpoint_spec.rb
+++ spec/requests/api/v1/meals/meals_endpoint_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'GET /api/v1/meals' do
   it 'returns a list of meals and the associated foods' do
     meal = create(:meal_with_foods)
+    meal_2 = create(:meal_with_foods)
 
     expected = [{ 'id' => meal.id,
                  'name' => meal.name,
@@ -21,6 +22,26 @@ describe 'GET /api/v1/meals' do
                      'id' => meal.foods[2].id,
                      'name' => meal.foods[2].name,
                      'calories' => meal.foods[2].calories
+                   }
+                 ]
+               },
+               { 'id' => meal_2.id,
+                 'name' => meal_2.name,
+                 'foods' => [
+                   {
+                     'id' => meal_2.foods[0].id,
+                     'name' => meal_2.foods[0].name,
+                     'calories' => meal_2.foods[0].calories
+                   },
+                   {
+                     'id' => meal_2.foods[1].id,
+                     'name' => meal_2.foods[1].name,
+                     'calories' => meal_2.foods[1].calories
+                   },
+                   {
+                     'id' => meal_2.foods[2].id,
+                     'name' => meal_2.foods[2].name,
+                     'calories' => meal_2.foods[2].calories
                    }
                  ]
                }]
